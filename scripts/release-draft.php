09014235229583303ad383ea640d1e8afd14b886
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-27 18:22:41 -0300

    ci: move changelog and draft release checks to actions
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- scripts/release-draft.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$tag = getenv('RELEASE_TAG') ?: 'v0.1.0-draft';
-$notes = "Draft release generated from conventional workflow for {$tag}.";
-
-fwrite(STDOUT, $notes . PHP_EOL);
