fbe5151bfce9fe12a708e38d32560c9bb4ea4e0b
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-29 18:33:35 -0300

    refactor(pdf): add JpegPdfImageFactoryInterface
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ src/Pdf/JpegPdfImageFactoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreSign
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace LibreSign\XObjectTemplate\Pdf;
+
+/** @internal */
+interface JpegPdfImageFactoryInterface
+{
+    /**
+     * @param array<int|string, mixed> $imageInfo
+     */
+    public function create(string $contents, array $imageInfo): EmbeddedPdfImage;
+}
